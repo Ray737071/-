a43422c5b30076d48f8cbce0a1c2c4dbaf0503bf
Ray737071 <hkenboy@gmail.com>
2017-05-23 11:05:32 +0900

    #06変更、#07新規作成

--- #06.R
+++ #06.R
@@ -3,12 +3,10 @@ setwd("/Users/minemurarei/Documents/大学/３年/多変量解析/データ")
 
 x <- read.delim(pipe("pbpaste"))
 
-data <- read.csv(".csv",fileEncoding="cp932")
-head(data)
-dim(data)
+#PCAコマンド
+result <- princomp(x)
 
-reg <- lm( ~ , data)
-summary(full)
+summary(result,loadings = TRUE)
 
 
 #
